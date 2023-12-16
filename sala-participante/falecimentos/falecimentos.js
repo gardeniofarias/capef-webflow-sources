18a599f7a9811548526716ce3b7f57b1a91e967f
shahihika <batista.oliveira@rethink.dev>
2023-12-16 11:49:53 +0100

    feat: remove credentials

--- sala-participante/falecimentos/falecimentos.js
+++ sala-participante/falecimentos/falecimentos.js
@@ -114,8 +114,8 @@
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify({
-                    username: "Hero99",
-                    password: "d7OwsEqTXc",
+                    username: authUserName,
+                    password: authPassword,
                 }),
             }).then((result)=>result.json()).catch((error)=>console.log("🚀 ~ error:", error));
         }
